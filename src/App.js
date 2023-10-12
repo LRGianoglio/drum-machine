5d0af8ef6c809c6bc5d3dae394c119d201eed2ac
LRGianoglio <luchogianoglio@gmail.com>
2023-10-11 23:50:19 -0300

    feat/ basic redux implementation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { useSelector } from "react-redux";
+
 function App() {
+  const volume = useSelector(state => state.volume)
   return (
     <div className="App">
       <p>Holi :)</p>
+      <p>Volume {volume}</p>
     </div>
   );
 }
---
+++ src/redux/reducer.js
@@ -0,0 +1,23 @@
+import { CHANGE_VOLUME, POWER_SWITCH, BANK_SWITCH } from "./types"
+
+const initialState = {
+  lastPlayed: "",
+  volume: 50,
+  power: true,
+  bank: false
+}
+
+const rootReducer = (state = initialState, action) => {
+  switch(action.type){
+    case CHANGE_VOLUME:
+      return {...state, volume: action.payload}
+    case POWER_SWITCH:
+      return {...state, power: !state.power}
+    case BANK_SWITCH:
+      return {...state, power: !state.bank}
+    default:
+      return {...state}
+  }
+}
+
+export default rootReducer;
