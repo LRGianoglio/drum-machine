1612e13279e02a0b52913e5b10219ecaf6684ba2
unknown <luchogianoglio@gmail.com>
2023-10-19 11:45:40 -0300

    feat/ start with the switches

--- src/components/Switches/Switches.jsx
+++ src/components/Switches/Switches.jsx
@@ -3,7 +3,14 @@ import style from './Switches.module.css'
 function Switches() {
     return (
         <div className={style.switchesContainer}>
-            
+            <div className={style.switchContainer}>
+                <p className={style.label}>Power</p>
+                <input type="checkbox" className={style.input}/>
+            </div>
+            <div className={style.switchContainer}>
+                <p className={style.label}>Bank</p>
+                <input type="checkbox" />
+            </div>
         </div>
     );
 }
