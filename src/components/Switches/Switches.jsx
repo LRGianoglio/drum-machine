737101aab7acbf1f721714fc9355768712dff63a
unknown <luchogianoglio@gmail.com>
2023-10-20 09:59:49 -0300

    feat/ mostly finished styles

--- src/components/Switches/Switches.jsx
+++ src/components/Switches/Switches.jsx
@@ -1,15 +1,19 @@
 import style from './Switches.module.css'
+import {FiPower} from 'react-icons/fi'
+import { Switch, FormControlLabel } from '@mui/material';
 
 function Switches() {
     return (
         <div className={style.switchesContainer}>
             <div className={style.switchContainer}>
-                <p className={style.label}>Power</p>
-                <input type="checkbox" className={style.input}/>
+                <button className={style.powerButton}>
+                    <p className={style.label}>Power</p>
+                    <FiPower className={style.powerIcon}/>
+                </button>
             </div>
             <div className={style.switchContainer}>
                 <p className={style.label}>Bank</p>
-                <input type="checkbox" />
+                <Switch color='default' className={style.bankSwitch} />
             </div>
         </div>
     );
